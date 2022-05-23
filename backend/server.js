6ece9f91d3dd46db8f760f715e91bb846f9e9673
Marko Lazarevic <mare99lazarevic@gmail.com>
2022-05-23 16:00:47 +0200

    some error occured

--- backend/server.js
+++ backend/server.js
@@ -13,14 +13,6 @@ app.get("/", (req,res) => {
 
 app.use('/api/products', productRoutes)
 
-// app.get("/api/products", (req,res) => {
-//     res.json(products)
-// })
-
-// app.get("/api/products/:id", (req,res) => {
-//     const product = products.find((p) => p._id === req.params.id)
-//     res.json(product)
-// })
 
 const port = process.env.PORT || 5000
 app.listen(port, console.log(`Servee running in ${process.env.NODE_ENV} mode on ${port}`))
